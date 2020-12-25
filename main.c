9c7260749d0cf3efe7baac46bbc22c280519ea7f
Rohit Luthra <rohitluthra9@outlook.com>
2020-12-24 20:04:23 -0500

    Virtualization Example added

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "common.h"
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int main ( int argc, char *argv[] ) {
+
+    if (argc != 2)
+    {
+        printf("Invalid Input \n");
+        exit(1);
+    }
+
+    int *p;
+
+    p = malloc (sizeof(int));
+    assert(p!= NULL);
+
+    printf("(%d) address of pointer to by p: %p \n", (int) getpid(), p);
+
+    *p =  atoi (argv[1]); // assign value to address stored in p
+
+    while(1)
+    {
+        Spin(1);
+        *p = *p + 1;
+        printf("(%d value of p: %p \n)", (int) getpid(), p);
+    }
 }
