f3eadadbadc1f574420080239881a1c7f936eb66
Rohit Luthra <rohitluthra9@outlook.com>
2020-12-26 19:11:04 -0500

    forking example

---
+++ Process_Creation_API/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main (int argc, char * argv [])
+{
+
+  /*
+  What is fflush ?
+
+  printf("Enter a number: ");   // print won't appear on console
+  printf("Enter a number:\n");  // with \n, it does show on console.
+
+  - The reason for this is that the output to stdout is buffered by the OS and the default behavior is (often) only to actually
+  write the output to the terminal when a newline is encountered.
+
+  - Adding an fflush(stdout) after the printf() solves the problem:
+  */
+  fflush (stdout);
+  printf("Initial (pid:%d)\n", (int)getpid());
+
+  int rc = fork() ;
+  if (rc < 0)
+  {
+    // fork has failed
+    printf("Fork has Failed \n");
+    exit (1);
+  }
+  else if (rc == 0)
+  {
+    // You are in child Process
+    printf("Child process successfully created , it's PID is : %d \n", getpid());
+  }
+  else{
+
+    // Parent process entry path
+    printf("Parent Process successfully created, it's PID is %d \n", getpid());
+  }
+
+  return 0;
+}
